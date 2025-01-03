53880aae21d259c6472df98bbff3f0e8bc6e5e4b
artender <artem1347929@gmail.com>
2025-01-03 20:34:35 +0300

    🧪 Add user profile view by username tests

--- tests/test_users/test_profiles.py
+++ tests/test_users/test_profiles.py
@@ -3,6 +3,7 @@ import pytest
 from fastapi.testclient import TestClient
 from app.main import app
 from tests.utils import AuthorizationData
+from tests.test_users.utils import test_credentials
 
 client = TestClient(app)
 
@@ -14,15 +15,7 @@ def get_user_profile(
     response = client.get(endpoint, headers=authorization.header)
     return cast(dict[str, str], response.json())
 
-@pytest.mark.parametrize(
-    "authorization",
-    [
-        {"username": "john_doe", "password": "SuperSecret"},
-        {"username": "JaneDoe", "password": "LoremIpsum"},
-        {"username": "mysterious_hacker", "password": "LiterallyUnhackablePassword"}
-    ],
-    indirect=True
-)
+@pytest.mark.parametrize("authorization", test_credentials, indirect=True)
 def test_retrieving_current_user_profile(authorization: AuthorizationData) -> None:
     user_profile = get_user_profile(authorization)
     assert user_profile == {
@@ -48,7 +41,19 @@ def test_updating_current_user_profile(
         user_profile_before_update | update_fields
     )
     response = client.put("/users/me", headers=authorization.header, json=update_fields)
-    assert response.status_code == 200
     assert response.json() == expected_user_profile_after_update
     user_profile_after_update = get_user_profile(authorization)
     assert user_profile_after_update == expected_user_profile_after_update
+
+@pytest.mark.parametrize("register_credentials", test_credentials)
+def test_retrieving_specific_user_profile_by_username(
+    authorization: AuthorizationData,
+    register_credentials: dict[str, str]
+) -> None:
+    client.post("/register", json=register_credentials)
+    user_profile = get_user_profile(authorization, register_credentials["username"])
+    assert user_profile == {
+        "username": register_credentials["username"],
+        "bio": None,
+        "totalPosts": 0
+    }
---
+++ tests/test_users/utils.py
@@ -0,0 +1,5 @@
+test_credentials = [
+    {"username": "john_doe", "password": "SuperSecret"},
+    {"username": "JaneDoe", "password": "LoremIpsum"},
+    {"username": "mysterious_hacker", "password": "LiterallyUnhackablePassword"}
+]
