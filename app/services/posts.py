53a0e5a5efbc4f3d515942ca4588d2742c6c9719
artender <artem1347929@gmail.com>
2025-01-04 14:47:32 +0300

    ✨ Add total posts incrementing on post creation

--- app/services/posts.py
+++ app/services/posts.py
@@ -1,21 +1,28 @@
 from datetime import datetime
-from tinydb import TinyDB
+from tinydb import TinyDB, where
+from tinydb.operations import increment
 from app.schemas.posts import PostWithMetaData
 
 class PostsService:
     def __init__(self) -> None:
         self.posts_db = TinyDB("app/db/posts.json", indent=4)
+        self.users_db = TinyDB("app/db/users.json", indent=4)
 
     def create_post(self, title: str, author: str, content: str) -> PostWithMetaData:
         post = {
             "title": title,
             "author": author,
             "content": content,
-            "timestamp": self._generate_timestamp()
+            "timestamp": PostsService._generate_timestamp()
         }
         post_id = self.posts_db.insert(post)
         self.posts_db.update({"postId": post_id}, doc_ids=[post_id])
+        self._increment_author_total_posts(author)
         return PostWithMetaData(**post, post_id=post_id)
+    
+    def _increment_author_total_posts(self, author: str) -> None:
+        self.users_db.update(increment("totalPosts"), where("username") == author) # type: ignore
 
-    def _generate_timestamp(self) -> str:
+    @staticmethod
+    def _generate_timestamp() -> str:
         return datetime.now().isoformat()
